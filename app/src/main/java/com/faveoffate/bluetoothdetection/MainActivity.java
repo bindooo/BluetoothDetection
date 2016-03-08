0472c11136b5d979732f51cb8263b8f5b897efc1
bindooo <bindicsgeri@gmail.com>
2016-03-08 18:01:43 +0100

    Code refactored

--- app/src/main/java/com/faveoffate/bluetoothdetection/MainActivity.java
+++ app/src/main/java/com/faveoffate/bluetoothdetection/MainActivity.java
@@ -3,32 +3,22 @@ package com.faveoffate.bluetoothdetection;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Set;
@@ -40,83 +30,36 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mSocket;
-    TelephonyManager tManager;
     String uuid;
     UUID MY_UUID;
     Set<BluetoothDevice> pairedDevices;
-    int rssi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mHandler = new Handler();
-        tv = (TextView)findViewById(R.id.textView);
+        tv = (TextView) findViewById(R.id.textView);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         mSocket = null;
 
-        uuid ="0000111f-0000-1000-8000-00805f9b34fb";
-/*
-        ///getting own phone's uuids
-        Method getUuidsMethod = null;
-        try {
-            getUuidsMethod = BluetoothAdapter.class.getDeclaredMethod("getUuids", null);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-
-        ParcelUuid[] uuids = new ParcelUuid[0];
-        try {
-            uuids = (ParcelUuid[]) getUuidsMethod.invoke(mBluetoothAdapter, null);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-        ///logging them
-        for (ParcelUuid uuid: uuids) {
-            Log.d("Device: ", "UUID: " + uuid.getUuid().toString());
-        }
-*/
-
+        uuid = "0000111f-0000-1000-8000-00805f9b34fb";
         MY_UUID = UUID.fromString(uuid);
 
         if (mBluetoothAdapter == null) {
             tv.setText("No Bluetooth for you :(");
             Log.d("Device info: ", "No Bluetooth for you :(");
-        }
-        else {/*
-            date = dateformat.format(c.getTime());
-            time = timeformat.format(c.getTime());
-            Log.d("Current date ", date);
-            Log.d("Current time ", time);
-*/
-//            SendHTTPRequest sr = new SendHTTPRequest();
-//            sr.execute(tv);
+        } else
             //enableBluetooth();
-            //mHandler.postDelayed(new Runnable() {
-                //public void run() {
             startTaskLoop();
-                    //findPairedDevices();
-                    //IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                    //registerReceiver(mReceiver, filter);
-                    //mBluetoothAdapter.startDiscovery();
-                    //Log.d("Handler","Task repetition");
-                //}
-            //}, 5000);
-        }
     }
+
     Runnable mStatusChecker = new Runnable() {
         @Override
         public void run() {
-            try {
-
-            } finally {
-                //findPairedDevices();
-                Log.d("Handler","Task repetition");
-                mHandler.postDelayed(mStatusChecker, 5000);
-            }
+            findPairedDevices();
+            Log.d("Handler", "Task repetition running");
+            mHandler.postDelayed(mStatusChecker, 5000);
         }
     };
 
@@ -127,12 +70,14 @@ public class MainActivity extends AppCompatActivity {
     void stopTaskLoop() {
         mHandler.removeCallbacks(mStatusChecker);
     }
+
     protected void enableBluetooth() {
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
     }
+
     protected void findPairedDevices() {
         pairedDevices = mBluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
@@ -141,85 +86,59 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     mSocket = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
                 } catch (IOException e1) {
-                    Log.d("Socket: ","Socket not created");
+                    Log.d("Socket: ", "Socket not created");
                     e1.printStackTrace();
                 }
-                //device.getUuids();
-                try{
+                try {
                     mSocket.connect();
                     Log.d("Connect: ", "Connected");
                     try {
                         mSocket.close();
                         Log.d("Connect: ", "Disconnected, socket closed");
                     } catch (IOException e1) {
-                        Log.d("Closed: ","Socket not closed 1");
+                        Log.d("Closed: ", "Socket not closed 1");
                         e1.printStackTrace();
                     }
                     Log.d("Connect: ", "Disconnected");
                     SendHTTPRequest sr = new SendHTTPRequest();
                     sr.execute(tv);
-                } catch(IOException e){
+                } catch (IOException e) {
                     try {
                         mSocket.close();
                         Log.d("Connect: ", "Cannot connect");
                         Log.d("Connect: ", "Socket closed");
                     } catch (IOException e1) {
-                        Log.d("Closed: ","Socket not closed 2");
+                        Log.d("Closed: ", "Socket not closed 2");
                         e1.printStackTrace();
                     }
                 }
             }
-        }
-        else
-            tv.setText("No paired devices :(");
+        } else
+            tv.setText("No paired devices!");
     }
-    protected void checkIfPaired(String foundMacAddress) {
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                if(device.getAddress().equals(foundMacAddress))
-                    Toast.makeText(MainActivity.this,Integer.toString(rssi), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                Log.d("Device:", device.getName() + " " + device.getAddress() + " " +rssi + "\n");
-                checkIfPaired(device.getAddress());
-            }
-        }
-    };
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         stopTaskLoop();
-        //unregisterReceiver(mReceiver);
-        //mBluetoothAdapter.cancelDiscovery();
     }
 
     private class SendHTTPRequest extends AsyncTask<TextView, Void, String> {
-        String date;
-        String time;
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-        SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
         TextView t;
         String responseStr = "fail";
+
         @Override
         protected String doInBackground(TextView... params) {
             this.t = params[0];
 
-            date = dateformat.format(c.getTime());
-            time = timeformat.format(c.getTime());
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
+            String date = dateformat.format(c.getTime());
+            String time = timeformat.format(c.getTime());
             Log.d("Current date ", date);
             Log.d("Current time ", time);
-
             String datetime = date + " " + time;
-
-//            String urlString = "http://balaton-team.com/bringa_send.php?" + date + "%20" + time;
 
             Uri.Builder builder = new Uri.Builder();
             builder.scheme("http")
@@ -231,7 +150,6 @@ public class MainActivity extends AppCompatActivity {
 
             URL url = null;
             try {
-                //url = new URL("http://balaton-team.com/bringa_send.php?id=phu&ts=2016-03-18%2011:12:00");
                 url = new URL(myUrl);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -267,6 +185,7 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(responseStr);
             return responseStr;
         }
+
         @Override
         protected void onPostExecute(String message) {
             super.onPostExecute(message);

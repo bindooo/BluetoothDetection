8f6bd47f5ca426105de77af5ebcd0fc2d9465f44
bindooo <bindicsgeri@gmail.com>
2016-04-02 16:01:16 +0200

    LE device search, nut found

--- app/src/main/java/com/faveoffate/bluetoothdetection/MainActivity.java
+++ app/src/main/java/com/faveoffate/bluetoothdetection/MainActivity.java
@@ -1,204 +1,114 @@
 package com.faveoffate.bluetoothdetection;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Set;
-import java.util.UUID;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_ENABLE_BT = 1;
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
     private Handler mHandler;
+
     TextView tv;
-    BluetoothAdapter mBluetoothAdapter;
-    BluetoothSocket mSocket;
-    String uuid, user;
-    UUID MY_UUID;
-    Set<BluetoothDevice> pairedDevices;
-    Context context;
+    String user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = getApplicationContext();
+        tv = (TextView) findViewById(R.id.textView2);
 
         Intent i = getIntent();
         user = i.getStringExtra(LoggedInActivity.EXTRA);
 
         mHandler = new Handler();
-        tv = (TextView) findViewById(R.id.textView);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        mSocket = null;
 
-        uuid = "0000111f-0000-1000-8000-00805f9b34fb";        //phone
-        //uuid = "00001803-0000-1000-8000-00805f9b34fb";          //nut
-        MY_UUID = UUID.fromString(uuid);
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) { //Check if BLE is supported
+            Toast.makeText(this, "Bluetooth Low Energy Not Supported!", Toast.LENGTH_SHORT).show(); //Message that BLE not supported
+            finish();                                                                   //End the app
+        }
 
-        if (mBluetoothAdapter == null) {
-            tv.setText("No Bluetooth for you :(");
-            Log.d("Device info: ", "No Bluetooth for you :(");
-        } else
-            //enableBluetooth();
-            startTaskLoop();
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE); //Get the BluetoothManager
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (mBluetoothAdapter == null) {                                                //Check if we got the BluetoothAdapter
+            Toast.makeText(this, "Bluetooth Not Supported!", Toast.LENGTH_SHORT).show(); //Message that Bluetooth not supported
+            finish();                                                                   //End the app
+        }
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) { //User chose not to enable Bluetooth.
+            finish();                                                                   //Destroy the activity - end the application
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);                          //Pass the activity result up to the parent method
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    Runnable mStatusChecker = new Runnable() {
+        if (!mBluetoothAdapter.isEnabled()) {                                           //Check if BT is not enabled
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); //Create an intent to get permission to enable BT
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);                  //Fire the intent to start the activity that will return a result based on user response
+        }
+
+        scanLeDevice(true);                                                             //Start scanning for BLE devices
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);                                                            //Stop scanning for BLE devices         //Clear the list of BLE devices found during the scan
+    }
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {                                                                   //Method was called with option to start scanning
+            mHandler.postDelayed(new Runnable() {                                       //Create delayed runnable that will stop the scan when it runs after SCAN_PERIOD milliseconds
+                @Override
+                public void run() {
+                    mScanning = false;                                                  //Indicate that we are not scanning - used for menu Stop/Scan context
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);                      //Stop scanning - callback method indicates which scan to stop
+                    invalidateOptionsMenu();                                            //Indicate that the options menu has changed, so should be recreated.
+                }
+            }, 10000);
+
+            mScanning = true;                                                           //Indicate that we are busy scanning - used for menu Stop/Scan context
+            mBluetoothAdapter.startLeScan(mLeScanCallback);                             //Start scanning with callback method to execute when a new BLE device is found
+        }
+        else {                                                                          //Method was called with option to stop scanning
+            mScanning = false;                                                          //Indicate that we are not scanning - used for menu Stop/Scan context
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);                              //Stop scanning - callback method indicates which scan to stop
+        }
+        invalidateOptionsMenu();                                                        //Indicate that the options menu has changed, so should be recreated.
+    }
+    private final BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
         @Override
-        public void run() {
-            findPairedDevices();
-            Log.d("Handler", "Task repetition running");
-            mHandler.postDelayed(mStatusChecker, 5000);
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) { //Android calls method with Bluetooth device advertising information
+            runOnUiThread(new Runnable() {                                              //Create runnable that will add the device to the list adapter
+                @Override
+                public void run() {
+                    tv.append(device.getName() + " " + device.getAddress() + "\n");
+                    Log.d(TAG, "Found BLE Device: " + device.getAddress()); //Debug information to log the devices as they are found
+                    if (device.getAddress().equals("D5:10:43:0B:99:2F")) {
+                        scanLeDevice(false);
+                        Toast.makeText(MainActivity.this, "Little nut found!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     };
-
-    void startTaskLoop() {
-        mStatusChecker.run();
-    }
-
-    void stopTaskLoop() {
-        mHandler.removeCallbacks(mStatusChecker);
-    }
-
-    protected void enableBluetooth() {
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-    }
-
-    protected void findPairedDevices() {
-        pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                Log.d("Device: ", device.getName() + device.getAddress() + "\n");
-                try {
-                    mSocket = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
-                } catch (IOException e1) {
-                    Log.d("Socket: ", "Socket not created");
-                    e1.printStackTrace();
-                }
-                try {
-                    mSocket.connect();
-                    Log.d("Connect: ", "Connected");
-                    try {
-                        mSocket.close();
-                        Log.d("Connect: ", "Disconnected, socket closed");
-                    } catch (IOException e1) {
-                        Log.d("Closed: ", "Socket not closed 1");
-                        e1.printStackTrace();
-                    }
-                    Log.d("Connect: ", "Disconnected");
-
-                    Calendar c = Calendar.getInstance();
-                    SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-                    SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
-                    String date = dateformat.format(c.getTime());
-                    String time = timeformat.format(c.getTime());
-                    String datetime = date + " " + time;
-                    tv.append(user + " " + datetime);
-                    tv.append("\n");
-
-                    try {
-                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("timestamps.txt", Context.MODE_PRIVATE));
-                        outputStreamWriter.write(tv.getText().toString());
-                        outputStreamWriter.write("\n");
-                        outputStreamWriter.close();
-                    } catch (IOException e) {
-                        Log.d("Error: ", e.toString());
-                    }
-
-                    SendHTTPRequest sr = new SendHTTPRequest();
-                    sr.execute(tv);
-                } catch (IOException e) {
-                    try {
-                        mSocket.close();
-                        Log.d("Connect: ", "Cannot connect");
-                        Log.d("Connect: ", "Socket closed");
-                    } catch (IOException e1) {
-                        Log.d("Closed: ", "Socket not closed 2");
-                        e1.printStackTrace();
-                    }
-                }
-            }
-        } else
-            tv.setText("No paired devices!");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        stopTaskLoop();
-    }
-
-    private class SendHTTPRequest extends AsyncTask<TextView, Void, String> {
-        TextView t;
-        String responseStr = "fail";
-
-        @Override
-        protected String doInBackground(TextView... params) {
-            this.t = params[0];
-
-            Calendar c = Calendar.getInstance();
-            SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-            SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
-            String date = dateformat.format(c.getTime());
-            String time = timeformat.format(c.getTime());
-            Log.d("Current date ", date);
-            Log.d("Current time ", time);
-            String datetime = date + " " + time;
-
-            Uri.Builder builder = new Uri.Builder();
-            builder.scheme("http")
-                    .authority("balaton-team.com")
-                    .appendPath("bringa_send.php")
-                    .appendQueryParameter("id", "phu")
-                    .appendQueryParameter("ts", datetime);
-            String myUrl = builder.build().toString();
-
-            URL url;
-            try {
-                url = new URL(myUrl);
-                HttpURLConnection conn;
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                System.out.println("Response Code: " + conn.getResponseCode());
-                InputStream in;
-                in = new BufferedInputStream(conn.getInputStream());
-                responseStr = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println(responseStr);
-            return responseStr;
-        }
-
-        @Override
-        protected void onPostExecute(String message) {
-            super.onPostExecute(message);
-            //t.setText(message);
-        }
-    }
 }

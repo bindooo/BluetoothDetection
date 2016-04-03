25bcc3a9b77657e83d189649f31338365f1445c0
bindooo <bindicsgeri@gmail.com>
2016-04-03 14:49:56 +0200

    Append to tv and tf if nut found

--- app/src/main/java/com/faveoffate/bluetoothdetection/MainActivity.java
+++ app/src/main/java/com/faveoffate/bluetoothdetection/MainActivity.java
@@ -14,6 +14,11 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_ENABLE_BT = 1;
@@ -23,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tv;
     String user;
+    Context context;
 
+    Calendar c = Calendar.getInstance();
+    SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm:ss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +40,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv = (TextView) findViewById(R.id.textView2);
-
         Intent i = getIntent();
         user = i.getStringExtra(LoggedInActivity.EXTRA);
+        context = getApplicationContext();
 
         mHandler = new Handler();
 
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) { //Check if BLE is supported
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {           //Check if BLE is supported
             Toast.makeText(this, "Bluetooth Low Energy Not Supported!", Toast.LENGTH_SHORT).show(); //Message that BLE not supported
-            finish();                                                                   //End the app
+            finish();                                                                               //End the app
         }
 
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE); //Get the BluetoothManager
         mBluetoothAdapter = bluetoothManager.getAdapter();
 
-        if (mBluetoothAdapter == null) {                                                //Check if we got the BluetoothAdapter
-            Toast.makeText(this, "Bluetooth Not Supported!", Toast.LENGTH_SHORT).show(); //Message that Bluetooth not supported
-            finish();                                                                   //End the app
+        if (mBluetoothAdapter == null) {                                                    //Check if we got the BluetoothAdapter
+            Toast.makeText(this, "Bluetooth Not Supported!", Toast.LENGTH_SHORT).show();    //Message that Bluetooth not supported
+            finish();                                                                       //End the app
         }
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) { //User chose not to enable Bluetooth.
-            finish();                                                                   //Destroy the activity - end the application
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {   //User chose not to enable Bluetooth.
+            finish();                                                                       //Destroy the activity - end the application
             return;
         }
-        super.onActivityResult(requestCode, resultCode, data);                          //Pass the activity result up to the parent method
+        super.onActivityResult(requestCode, resultCode, data);                              //Pass the activity result up to the parent method
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -69,11 +80,13 @@ public class MainActivity extends AppCompatActivity {
 
         scanLeDevice(true);                                                             //Start scanning for BLE devices
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        scanLeDevice(false);                                                            //Stop scanning for BLE devices         //Clear the list of BLE devices found during the scan
+        scanLeDevice(false);                                                            //Stop scanning for BLE devices
     }
+
     private void scanLeDevice(final boolean enable) {
         if (enable) {                                                                   //Method was called with option to start scanning
             mHandler.postDelayed(new Runnable() {                                       //Create delayed runnable that will stop the scan when it runs after SCAN_PERIOD milliseconds
@@ -87,28 +100,49 @@ public class MainActivity extends AppCompatActivity {
 
             mScanning = true;                                                           //Indicate that we are busy scanning - used for menu Stop/Scan context
             mBluetoothAdapter.startLeScan(mLeScanCallback);                             //Start scanning with callback method to execute when a new BLE device is found
-        }
-        else {                                                                          //Method was called with option to stop scanning
+        } else {                                                                          //Method was called with option to stop scanning
             mScanning = false;                                                          //Indicate that we are not scanning - used for menu Stop/Scan context
             mBluetoothAdapter.stopLeScan(mLeScanCallback);                              //Stop scanning - callback method indicates which scan to stop
         }
         invalidateOptionsMenu();                                                        //Indicate that the options menu has changed, so should be recreated.
     }
+
     private final BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
 
         @Override
-        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) { //Android calls method with Bluetooth device advertising information
+        public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {   //Android calls method with Bluetooth device advertising information
             runOnUiThread(new Runnable() {                                              //Create runnable that will add the device to the list adapter
                 @Override
                 public void run() {
-                    tv.append(device.getName() + " " + device.getAddress() + "\n");
-                    Log.d(TAG, "Found BLE Device: " + device.getAddress()); //Debug information to log the devices as they are found
+                    //tv.append(device.getName() + " " + device.getAddress() + "\n");
+                    Log.d(TAG, "Found BLE Device: " + device.getAddress());                 //Debug information to log the devices as they are found
                     if (device.getAddress().equals("D5:10:43:0B:99:2F")) {
                         scanLeDevice(false);
-                        Toast.makeText(MainActivity.this, "Little nut found!", Toast.LENGTH_SHORT).show();
+                        appendToTextView();
+                        appendToTextFile();
+                        //Toast.makeText(MainActivity.this, "Little nut found!", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }
     };
+
+    private void appendToTextView() {
+        String date = dateformat.format(c.getTime());
+        String time = timeformat.format(c.getTime());
+        String datetime = date + " " + time;
+        tv.append(user + " " + datetime);
+        tv.append("\n");
+    }
+
+    private void appendToTextFile() {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("timestamps.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(tv.getText().toString());
+            outputStreamWriter.write("\n");
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.d("Error: ", e.toString());
+        }
+    }
 }
